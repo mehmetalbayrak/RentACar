87aa9d2e6034b35821501a56a1678d8530018b4c
Mehmet Albayrak <mehmet__albayrak@outlook.com>
2023-08-12 22:49:32 +0300

    added pagination

--- WebApi/Controllers/BrandsController.cs
+++ WebApi/Controllers/BrandsController.cs
@@ -1,4 +1,7 @@
 ﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Queries.GetList;
+using Core.Application.Requests;
+using Core.Application.Responses;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +12,16 @@ namespace WebApi.Controllers
     public class BrandsController : BaseController
     {
         [HttpPost]
-        public async Task<IActionResult> Add([FromBody]CreateBrandCommand createBrandCommand)
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
         {
-           CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+            CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+            return Ok(response);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
+            GetListResponse<GetListBrandListItemDto> response = await Mediator.Send(getListBrandQuery);
             return Ok(response);
         }
     }
